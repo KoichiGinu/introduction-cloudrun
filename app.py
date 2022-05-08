88b62586a303514dad91f413bfb577962fa36175
KoichiGinu <53080776+KoichiGinu@users.noreply.github.com>
2022-05-08 17:01:29 +0900

    cats api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 import math
+import requests
 import random
 
 app = Flask(__name__)
@@ -11,10 +12,21 @@ app = Flask(__name__)
 def hello_world():
     return "Hello CloudRun"
 
+# ランダムな数字を返す(0-100)
 @app.route('/rand')
 def rand():
     num = math.floor(random.uniform(0, 100))
     return str(num)
+
+# 猫画像を返す
+# The Cat API (https://thecatapi.com/)
+@app.route('/cats')
+def cats():
+    url = "https://api.thecatapi.com/v1/images/search"
+    headers = {'x-api-key': 'hogehoge'}
+    result = requests.get(url, headers=headers)
+    jsonData = result.json()
+    return "<img src=" + jsonData[0]["url"] + " width=500>"
 
 
 if __name__ == "__main__":
